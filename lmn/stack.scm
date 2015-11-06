00c8236b94ff4701c7feac9bdad89f34e4d3ba69
p <_>
2015-11-06 18:54:12 +0900

    add stack.scm

---
+++ lmn/stack.scm
@@ -0,0 +1,58 @@
+(define-module lmn.stack
+  (export <stack> make-stack stack-push! stack-pop!
+          stack-length stack-empty? stack-ref))
+
+(select-module lmn.stack)
+
+;; スタックを提供する。このライブラリの提供するスタックは push/pop 以外
+;; の方法で変更することができない代わりに、要素へのランダムアクセスがで
+;; きる。内部的には Java の ArrayList のように適宜拡大される配列で、ア
+;; ロケーションユニットごとに全体がコピーされる。
+
+(define-class <stack> ()
+  ;; スタックのオブジェクト。
+  ((alloc-unit :init-keyword :alloc-unit) ;; Nat
+   (data :init-keyword :data)             ;; Vector
+   (length :init-keyword :length)))       ;; Int
+
+(define (make-stack :optional [alloc-unit 30])
+  ;; スタックを作成する。
+  (make <stack> :alloc-unit alloc-unit :data (make-vector alloc-unit) :length 0))
+
+(define (stack-push! stack obj)
+  ;; STACK にオブジェクト OBJ を push する。
+  (let ([length (slot-ref stack 'length)]
+        [data (slot-ref stack 'data)])
+    ;; 配列が小さければ拡大する
+    (when (= length (vector-length data))
+      (let1 newdata (vector-copy data 0 (+ length (slot-ref stack 'alloc-unit)))
+        (slot-set! stack 'data newdata)
+        (set! data newdata)))
+    ;; オブジェクトを追加
+    (vector-set! data length obj)
+    (slot-set! stack 'length (+ 1 length))))
+
+(define (stack-pop! stack :optional [n 1])
+  ;; STACK からオブジェクトを N つ捨てる。 push した数以上のオブジェク
+  ;; トを捨てるとエラーになる。
+  (let ([length (slot-ref stack 'length)]
+        [data (slot-ref stack 'data)])
+    (when (< length n)
+      (error "Cannot operate pop for an empty stack."))
+    (slot-set! stack 'length (- length n))))
+
+(define (stack-length stack)
+  ;; STACK に含まれる要素の数を返す。
+  (slot-ref stack 'length))
+
+(define (stack-empty? stack)
+  ;; STACK が空の場合に限り #f でない値を返す。
+  (= (slot-ref stack 'length) 0))
+
+(define (stack-ref stack n)
+  ;; STACK に N 番目 (0-origin) に push されたオブジェクトを返す。その
+  ;; ようなオブジェクトがない場合にはエラーになる。
+  (unless (and (>= n 0)
+               (< n (slot-ref stack 'length)))
+    (error "Stack boundary error."))
+  (vector-ref (slot-ref stack 'data) n))
