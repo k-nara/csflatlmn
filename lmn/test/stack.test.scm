1c6d07136f6cd055d5c8ddc0f6630c2eddde853e
p <_>
2015-11-06 18:54:20 +0900

    add tests for stack.scm

---
+++ lmn/test/stack.test.scm
@@ -0,0 +1,29 @@
+(use gauche.test)
+(use lmn.test.util)
+
+(test-start "lmn.stack")
+
+(test-module 'lmn.stack)
+
+;; ----------------------
+
+(define stack (make-stack))
+
+(test* "stack-empty? (1)" #t (stack-empty? stack))
+
+(stack-push! stack 'a)
+(stack-push! stack 'b)
+(stack-push! stack 'c)
+(stack-push! stack 'd)
+
+(test* "stack-empty? (2)" #f (stack-empty? stack))
+(test* "stack-length" 4 (stack-length stack))
+(test* "stack-ref (1)" 'c (stack-ref stack 2))
+(test* "stack-ref (2)" (test-error <error>) (stack-ref stack 4))
+
+(stack-pop! stack)
+(stack-pop! stack 2)
+
+(test* "stack-pop!" 1 (stack-length stack))
+
+(test-end :exit-on-failure #t)
--- test.scm
+++ test.scm
@@ -6,8 +6,10 @@
 (use lmn.atom)
 (use lmn.atomset)
 (use lmn.process)
+(use lmn.stack)
 
 (load "./lmn/test/error.test.scm")
 (load "./lmn/test/atom.test.scm")
 (load "./lmn/test/atomset.test.scm")
 (load "./lmn/test/process.test.scm")
+(load "./lmn/test/stack.test.scm")
