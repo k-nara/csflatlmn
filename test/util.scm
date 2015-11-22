725197e19b49f0f1320d245a3738b4ccc75a2e1b
p <_>
2015-11-22 22:06:49 +0900

    refactoring

--- test/util.scm
+++ test/util.scm
@@ -1,17 +1,21 @@
 (define-module test.util
-  (export set-equal? list-equal? generator->list))
+  (export set-equal? list-equal? boolean-equal? generator->list))
 
 (select-module test.util)
 
 ;; ２つのリストを集合として、関数 COMP の意味で比較する。
-(define ((set-equal? comp) s1 s2)
+(define ((set-equal? :optional [comp equal?]) s1 s2)
   (and (every (cut member <> s2 comp) s1) (every (cut member <> s1 comp) s2)))
 
 ;; ２つのリストを関数 COMP の意味で比較する。
-(define ((list-equal? comp) s1 s2)
+(define ((list-equal? :optional [comp equal?]) s1 s2)
   (let loop ([s1 s1] [s2 s2])
     (or (and (null? s1) (null? s2))
         (and (pair? s1) (pair? s2) (comp (car s1) (car s2)) (loop (cdr s1) (cdr s2))))))
+
+;; ２つのオブジェクトの真偽を比較する
+(define (boolean-equal? a b)
+  (if a b (not b)))
 
 ;; イテレータからリストを作る
 (define (generator->list gen)
