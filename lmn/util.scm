46ada0b028951c510d0506736c403e9b6040e27e
p <_>
2015-11-24 12:38:37 +0900

    add push/pop function to `dump`

--- lmn/util.scm
+++ lmn/util.scm
@@ -4,16 +4,20 @@
 (select-module lmn.util)
 
 (define *debug* #f)
-(define *debug-level* 1)
+(define *debug-level* '(0))
 
 ;; もし *debug* が #f でないなら、デバッグメッセージを出力する。 デバッ
 ;; グメッセージの次の行のインデントは整数 DLEVEL のぶんだけ深くなる。
-;; ARGS は表示するオブジェクトの列。
+;; ARGS は表示するオブジェクトの列。 DLEVEL が 'push の場合、現在のイン
+;; デントレベルをスタックにプッシュする。 'pop の場合、前回プッシュされ
+;; たインデントレベルに戻す。
 (define (dump dlevel :rest args)
   (when *debug*
-    (apply print (append (make-list (max *debug-level* 0) " | ")
-                         (map x->string args)))
-    (inc! *debug-level* dlevel)
+    (apply print (append (make-list (max (car *debug-level*) 0) " | ") (map x->string args)))
+    (case dlevel
+      [(push) (push! *debug-level* (car *debug-level*))]
+      [(pop) (pop! *debug-level*)]
+      [else (inc! (car *debug-level*) dlevel)])
     (flush)))
 
 ;; "delete!" に似ているが、多くとも１つの要素しか削除しない。重複のない
