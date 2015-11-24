d36eed1ed817572226ddbbb0f075fa9d6ecf1783
p <_>
2015-11-24 12:39:11 +0900

    implement `alist-delete1!` function

--- lmn/util.scm
+++ lmn/util.scm
@@ -1,5 +1,5 @@
 (define-module lmn.util
-  (export *debug* dump delete1! set-equal? list-equal?))
+  (export *debug* *debug-level* dump delete1! alist-delete1!))
 
 (select-module lmn.util)
 
@@ -26,6 +26,20 @@
   (let loop ([left ()] [right lst])
     (cond [(null? right) lst]
           [(not (elt=? (car right) x))
+           (loop right (cdr right))]
+          [(null? left)
+           (cdr right)]
+          [else
+           (set-cdr! left (cdr right))
+           lst])))
+
+;; "alist-delete!" に似ているが、多くとも１つのセルしか削除しない。重複
+;; のない連想リストではリストを最後まで走査しないぶん "alist-delete!"
+;; よりも高速。
+(define (alist-delete1! key lst :optional [elt=? equal?])
+  (let loop ([left ()] [right lst])
+    (cond [(null? right) lst]
+          [(not (elt=? (caar right) key))
            (loop right (cdr right))]
           [(null? left)
            (cdr right)]
