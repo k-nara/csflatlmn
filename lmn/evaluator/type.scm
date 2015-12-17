707aa7aa6ace31d2900e2b9aea4230d5be305fb7
p <_>
2015-12-17 16:11:30 +0900

    fix incorrect binding bug

--- lmn/evaluator/type.scm
+++ lmn/evaluator/type.scm
@@ -93,8 +93,8 @@
 ;; [LMNtal 構文で書かれた型]
 ;;
 ;; typedef same_len(T1, H1, T2, H2) {
+;;     H1 = T1, H2 = T2. ;; こっちを先に書かかないと効率が落ちる
 ;;     cons(Car1, Cdr1, H1), cons(Car2, Cdr2, H2) :- same_len(T1, Cdr1, T2, Cdr2).
-;;     H1 = T1, H2 = T2.
 ;; }
 
 ;; [型を整理 (このオブジェクトは作られない)]
@@ -103,14 +103,16 @@
 ;;     arity: 4
 ;;     rules: typerule{
 ;;                patterns:
-;;                subgoals: ("link" a1 a0), ("link" a3 a2) ;; 組込み型のみのものが先
+;;                bindings:
+;;                subgoals: "link", "link"
+;;                args:     (a1 a0) (a3 a2)
 ;;            }
 ;;            typerule {
-;;                patterns: (sexp->atomset '(("cons" 0 1 2))),
-;;                          (sexp->atomset '(("cons" 0 1 2)))
+;;                patterns: (sexp->atomset '(("cons" 0 1 2))), (sexp->atomset '(("cons" 0 1 2)))
 ;;                           l0 l1     l2 l3
 ;;                bindings: (#f #f a1) (#f #f a3)
-;;                subgoals: ("same_len" a0 1 a2 3) ;; 再帰するものは後
+;;                subgoals: "same_len"
+;;                args:     (a0 1 a2 3)
 ;;            }
 ;; }
 
@@ -118,18 +120,20 @@
 ;;
 ;; (make-type (make-type-rule 4
 ;;                            ()
+;;                            ()
 ;;                            '("link" "link")
 ;;                            '([(1) (0)] [(3) (2)]))
 ;;            (make-type-rule 4
-;;                            `(,(sexp->atomset '(("cons" 0 1 2))) ,(sexp->atomset '(("cons" 0 1 2))))
+;;                            `(,(sexp->atomset '(("cons" 0 1 2)))
+;;                              ,(sexp->atomset '(("cons" 0 1 2))))
+;;                            '([#f #f (1)] [#f #f (3)])
 ;;                            '("same_len")
-;;                            '([#f #f (1)] [#f #f (3)] [(0) 1 (2) 3])))
+;;                            '([(0) 1 (2) 3])))
 ;;
 ;; patterns はプロセステンプレートのリスト、 subgoals は型名のリスト、
-;; binding-template はパターンのバインディングとサブゴールの引数リスト
-;; をこの順で与える。それぞれは自然数, リストで囲まれた自然数, #f のリ
-;; ストで、リストで囲まれた自然数は型の引数を表す。サブゴールの引数リス
-;; トには #f は現れてはならない。
+;; pattern-bindings や subgoal-args はそれぞれ自然数 or リストで囲まれ
+;; た自然数 or #f のリストで、リストで囲まれた自然数は型の引数を表す。
+;; サブゴールの引数リストには #f は現れてはならない。
 
 ;; ---- type-check%
 
@@ -157,30 +161,33 @@
 ;; トの候補が複数ある場合がある。まだ候補が残っているときに next が #f
 ;; を返した場合、 LSTACK をもとに戻し、別の候補をプッシュし、あらためて
 ;; next を呼び出す。
-(define ((make-type-rule arity patterns subgoals binding-template) args)
+(define ((make-type-rule arity patterns pattern-bindings subgoals subgoal-args) args)
   ;; 引数の数を確認
   (unless (= arity (vector-length args))
     (error "(type-check) wrong number of arguments"))
   ;; binding-template の instantiate は静的にやっておく
   (let* ([count arity]
          [return-ix ()]
-         [binding (map (^x (map-to <vector>
-                                   (^y (cond [(not y) (inc! count) #f]
+         [patbinds (map (^x (map-to <vector>
+                                    (^y (cond [(not y) (inc! count) #f]
+                                              [(integer? y) (+ y arity)]
+                                              [(vector-ref args (car y)) (car y)]
+                                              [else (push! return-ix count)
+                                                    (inc! count)
+                                                    #f]))
+                                    x))
+                        pattern-bindings)]
+         [subargs (map (^x (map-to <vector>
+                                   (^y (cond [(not y) (inc! count 2) #f]
                                              [(integer? y) (+ y arity)]
-                                             [else (rlet1 ix (vector-ref args (car y))
-                                                     (unless ix
-                                                       (push! return-ix count)
-                                                       (inc! count)))]))
+                                             [(vector-ref args (car y)) (car y)]
+                                             [else (push! return-ix (+ 1 count))
+                                                   (inc! count 2)
+                                                   #f]))
                                    x))
-                       binding-template)]
-         [pp (apply seq% (let loop ([patterns patterns] [subgoals subgoals] [binding binding])
-                           (cond [(pair? patterns)
-                                  (cons (match-component% (car patterns) (car binding))
-                                        (loop (cdr patterns) subgoals (cdr binding)))]
-                                 [(pair? subgoals)
-                                  (cons (type-check% (car subgoals) (car binding))
-                                        (loop patterns (cdr subgoals) (cdr binding)))]
-                                 [else ()])))])
+                       subgoal-args)]
+         [pp (apply seq% (append! (map (^(p b) (match-component% p b)) patterns patbinds)
+                                  (map (^(s a) (type-check% s a)) subgoals subargs)))])
     (set! return-ix (reverse! return-ix))
     ;; ここから部分手続き
     (lambda% (proc known-atoms lstack pstack type-env)
